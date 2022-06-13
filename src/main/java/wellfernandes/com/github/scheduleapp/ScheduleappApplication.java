35e3e725c13171bbdbe6fcaaa4d402b1f57d4104
Welliton Fernandes Leal <wellfernandes@me.com>
2022-06-13 20:22:16 -0300

    project created

---
+++ src/main/java/wellfernandes/com/github/scheduleapp/ScheduleappApplication.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.scheduleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/wellfernandes/com/github/scheduleapp/ScheduleappApplicationTests.java
@@ -0,0 +1,13 @@
+package wellfernandes.com.github.scheduleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
